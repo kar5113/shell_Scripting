99d41aaf36e7da1c0afa85eece51392c7a140ed2
karthikeyareddyankireddy <karthikeya.ankireddy@rabobank.nl>
2025-11-01 21:17:14 +0530

    varaibles

--- variables.sh
+++ variables.sh
@@ -24,7 +24,8 @@ echo "Pleasure to meet you too, $person4! by $person2. and this script name is $
 #read -s means silent mode, input will not be shown on terminal
 #read -p means prompt mode, input will be shown on terminal
 #read without any option means normal mode, input will be shown on terminal
-read -p "Enter your password: "  password1
-read -s password2
-read password3
-echo "Your password is $password1 and $password2 and $password3"
+
+# read -p "Enter your password: "  password1
+# read -s password2
+# read password3
+# echo "Your password is $password1 and $password2 and $password3"
