7f6ea9588dd9c14325e445e3be4e6ee8f8063a8e
karthikeyareddyankireddy <karthikeya.ankireddy@rabobank.nl>
2025-11-01 17:48:24 +0530

    varaibles

--- conversation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "This is a conversation script by Ramesh."
-echo "How are you today? by suresh."
-echo "I hope you're doing well! by Ramesh."
-echo "I'm doing great, thank you! by suresh."
---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1="Ramesdfasfsh"
+person2="Sursdfsadfesh"
+
+echo "This is a conversation script by $person1."
+echo "How are you today? by $person2."
+echo "I hope you're doing well! by $person1."
+echo "I'm doing great, thank you! by $person2."
