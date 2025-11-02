f2abab896807a14b5097b76059cc3db8623155d1
karthikeyareddyankireddy <karthikeya.ankireddy@rabobank.nl>
2025-11-02 11:46:53 +0530

    ff

--- if_else.sh
+++ if_else.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+#### if else condition
+
 if [ $1 -gt 10 ]; then
     echo "The number is greater than 10"
 else
     echo "The number is not greater than 10"
 fi    
+
+
+#### if elif else condition
+
+if [ $1 -gt 20 ]; then
+    echo "The number is greater than 20"
+elif [ $1 -gt 10 ]; then
+    echo "The number is greater than 10 but less than or equal to 20"
+else
+    echo "The number is less than or equal to 10"
+fi
--- variables_notes.sh
+++ variables_notes.sh
@@ -119,6 +119,31 @@ echo "First Leader: ${LEADERS[0]}"
 echo "invalid index: ${LEADERS[10]}"
 
 
+#### conditional statements in shell script
+
+
+#if [ condition ]; then
+#   # commands
+#else
+#   # commands
+#fi
+
+if [ $1 -gt 10 ]; then
+    echo "The number is greater than 10"
+else
+    echo "The number is not greater than 10"
+fi  
+
+# if elif else condition
+
+if [ $1 -gt 20 ]; then
+    echo "The number is greater than 20"
+elif [ $1 -gt 10 ]; then
+    echo "The number is greater than 10 but less than or equal to 20"
+else
+    echo "The number is less than or equal to 10"
+fi
+
 
 
 
