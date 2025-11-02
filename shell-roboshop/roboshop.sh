0d3a8f56b514887b261ea84103913cb0e9ed9367
karthikeyareddyankireddy <karthikeya.ankireddy@rabobank.nl>
2025-11-02 18:12:11 +0530

    ww

--- shell-roboshop/roboshop.sh
+++ shell-roboshop/roboshop.sh
@@ -2,7 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SC_ID="sg-0e26215670f90eb6a"
-
+HOSTED_ZONE="Z0806995L2997E89SFOF"
+DOMAIN_NAME="kardev.space"
 
 for instance in $@; do 
     echo "creating ec2 instance for $instance" 
@@ -10,10 +11,15 @@ for instance in $@; do
 
 if [ $instance == "frontend" ]; then
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    RECORD_NAME="$DOMAIN_NAME"
 else
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    RECORD_NAME="$instance.$DOMAIN_NAME"
 fi
 
 echo "EC2 Instance $instance has been created with Instance ID: $INSTANCE_ID and IP Address: $IP"
 
+aws route53 change-resource-record-sets \
+    --hosted-zone-id $HOSTED_ZONE \
+    --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$RECORD_NAME'","Type":"A","TTL":3,"ResourceRecords":[{"Value":"'$IP'"}]}}]}'
 done
